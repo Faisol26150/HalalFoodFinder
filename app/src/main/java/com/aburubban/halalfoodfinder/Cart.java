a82074b0739f57ae4ae6cbb356d520c350c14e67
faisol26150 <faisolhayee135@gmail.com>
2019-01-24 01:15:39 +0700

    Payment by Paypal

--- app/src/main/java/com/aburubban/halalfoodfinder/Cart.java
+++ app/src/main/java/com/aburubban/halalfoodfinder/Cart.java
@@ -1,7 +1,9 @@
 package com.aburubban.halalfoodfinder;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aburubban.halalfoodfinder.Common.Common;
+import com.aburubban.halalfoodfinder.Common.Config;
 import com.aburubban.halalfoodfinder.Database.Database;
 import com.aburubban.halalfoodfinder.Model.MyResponse;
 import com.aburubban.halalfoodfinder.Model.Notification;
@@ -32,8 +35,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,6 +62,7 @@ import static android.os.Build.VERSION_CODES.N;
 
 public class Cart extends AppCompatActivity {
 
+    private static final int PAYPAL_REQUEST_CODE = 9999 ;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
@@ -64,6 +77,13 @@ public class Cart extends AppCompatActivity {
     CartAdapter adapter;
 
     APIService mService;
+
+    // Paypal
+    static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(Config.PAYPAL_CLIENT_ID);
+    String address,comment; //
+
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -81,6 +101,12 @@ public class Cart extends AppCompatActivity {
         .build());
 
         setContentView(R.layout.activity_cart);
+
+        //Init paypal
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        startService(intent);
+
 
         //Init Service
         mService = Common.getFCMService();
@@ -133,28 +159,25 @@ public class Cart extends AppCompatActivity {
         alertDialog.setPositiveButton("ยืนยัน", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                //Create new Request
-                Request request = new Request(
-                        Common.currentUser.getPhone(),
-                        Common.currentUser.getName(),
-                        edtAddress.getText().toString(),
-                        txtTotalPricce.getText().toString(),
-                        "0",//status
-                        edtComment.getText().toString(),
-                        cart
-                );
 
-                //Submit to Firebase
-                String order_number = String.valueOf(System.currentTimeMillis());
-                requests.child(order_number)
-                        .setValue(request);
+                //show paypal to payment
+                address = edtAddress.getText().toString();
+                comment = edtComment.getText().toString();
 
-                //Delete cart
-                new Database(getBaseContext()).cleanCart();
-                
-                sendNotificationOrder(order_number);
-               // Toast.makeText(Cart.this, "ขอบคุณสำหรับการสั่งซื้ออาหารกับเรา", Toast.LENGTH_SHORT).show();
-               // finish();
+                String formatAmount = txtTotalPricce.getText().toString()
+                                        .replace("THB","")
+                                        .replace(",","");
+
+                PayPalPayment  payPalPayment = new PayPalPayment(new BigDecimal(formatAmount),
+                        "THB",
+                        "ชำระรายการอาหาร",
+                        PayPalPayment.PAYMENT_INTENT_SALE);
+
+                Intent intent = new Intent(getApplicationContext(), PaymentActivity.class);
+                intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+                startActivityForResult(intent,PAYPAL_REQUEST_CODE);
+
 
             }
         });
@@ -168,6 +191,57 @@ public class Cart extends AppCompatActivity {
         });
 
         alertDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PAYPAL_REQUEST_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null)
+                {
+                    try {
+                        String paymentDetail = confirmation.toJSONObject().toString(4);
+                        JSONObject jsonObject = new JSONObject(paymentDetail);
+
+
+                //Create new Request
+                Request request = new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        address,
+                        txtTotalPricce.getText().toString(),
+                        "0",//status
+                        comment,
+                        jsonObject.getJSONObject("response").getString("state"),
+                        cart
+                );
+
+                //Submit to Firebase
+                String order_number = String.valueOf(System.currentTimeMillis());
+                requests.child(order_number)
+                        .setValue(request);
+
+                //Delete cart
+                new Database(getBaseContext()).cleanCart();
+
+                sendNotificationOrder(order_number);
+                Toast.makeText(Cart.this, "ขอบคุณสำหรับการสั่งซื้ออาหารกับเรา", Toast.LENGTH_SHORT).show();
+                finish();
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this, "ยกเลิกการชำระเงิน", Toast.LENGTH_SHORT).show();
+            else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+                Toast.makeText(this, "ไม่มีการชำระเงิน", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void sendNotificationOrder(final String order_number) {
---
+++ app/src/main/java/com/aburubban/halalfoodfinder/Common/Config.java
@@ -0,0 +1,9 @@
+package com.aburubban.halalfoodfinder.Common;
+
+/**
+ * Created by Abu Rubban on 23-Jan-19.
+ */
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AR9KNSIlEY1tSBq-7Di8vWjq5hXGR5r70SDkgDc5ThVtcfu4CMFqH6BFCSMv0uy9Zh-OAG0HZwWZ9WmZ";
+}
